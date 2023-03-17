204e37e3c166cd313bf6188f8bb77e9d3219102a
Zaituny <mohamedinv.580@gmail.com>
2023-03-17 15:08:38 -0700

    added 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a <= 'a'; a--)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
