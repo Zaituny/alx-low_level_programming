f9c6f5851fe48968d80e793dd84a787303915061
Zaituny <mohamedinv.580@gmail.com>
2023-03-17 16:03:39 -0700

    added 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int outerNumber, innerNumer;
+
+	for (outerNumber = 0; outerNumber <= 98; outerNumber++)
+	{
+		for (innerNumber = outerNumber + 1; innerNumber <= 99; innerNumber++)
+		{
+			putchar((outerNumber / 10) + '0');
+			putchar((outerNumber % 10) + '0');
+			putchar(' ');
+			putchar((innerNumber / 10) + '0');
+			putchar((innerNumber % 10) + '0');
+
+			if (outerNumber == 98 && innerNumber == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
