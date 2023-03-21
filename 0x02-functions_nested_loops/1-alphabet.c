0b1052caf5f19de0da24537569d132c331faa454
Zaituny <mohamedinv.580@gmail.com>
2023-03-21 14:16:39 -0700

    added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  prints the lowercase aplphabets
+ *
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
