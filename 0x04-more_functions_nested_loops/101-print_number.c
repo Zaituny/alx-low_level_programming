af7c764a3dbc6dabfc2a2024c6c21173f6283b60
Zaituny <mohamedinv.580@gmail.com>
2023-03-23 12:46:41 -0700

    added 101-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - prints number without using stdio
+ * @n: number to be printed
+ */
+void print_number(int n)
+{
+	int num = n;
+	
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+}
