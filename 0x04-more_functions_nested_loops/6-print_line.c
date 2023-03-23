514c879ab1d4b036228dcee03cfb78ffb7f1b4db
Zaituny <mohamedinv.580@gmail.com>
2023-03-23 11:27:34 -0700

    added 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_line - prints _ n times followed by a new line
+ * @n: number of _ to be printed
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = n; i >= 0; i--)
+		_putchar('_');
+	_putchar('\n');
+}
