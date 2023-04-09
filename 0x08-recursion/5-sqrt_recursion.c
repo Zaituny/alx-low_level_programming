a4f5c6053c7285f2126c30da847c245925df8110
Zaituny <mohamedinv.580@gmail.com>
2023-04-09 04:20:56 -0700

    added 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calculate square root
+ * @n: int to obtain the square root for
+ *
+ * Return: square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (helper_function(n, 0));
+}
+
+/**
+ * helper_function - calculates square root
+ * @n: number to get square root for
+ * @i: iterator
+ *
+ * Return: i if i is the square root
+ */
+int helper_function(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (helper_function(n, i + 1));
+}
