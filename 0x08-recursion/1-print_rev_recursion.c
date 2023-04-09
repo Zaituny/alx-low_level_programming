05563abfcdc569fb10b192ea755cd0f6828246ae
Zaituny <mohamedinv.580@gmail.com>
2023-04-09 03:38:50 -0700

    added 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * void _print_rev_recursion - print a string in reverse
+ * @s: string to be reversed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
