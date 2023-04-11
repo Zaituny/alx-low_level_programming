11e499b395b56a674fa34b20eac1a46085c37227
Zaituny <mohamedinv.580@gmail.com>
2023-04-11 14:07:40 -0700

    changed 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,6 +18,9 @@ char *create_array(unsigned int size, char c)
 
 	ar = (char *) malloc(sizeof(char) * size);
 
+	if (ar == NULL)
+		return (NULL);
+
 	for (i = 0; i <= size; i++)
 	{
 		if (i == size)
